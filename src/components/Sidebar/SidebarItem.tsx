0c5fd04dd2587b33204dbacd1235e218a424119d
Vinicius Holtman <viniholtman28@gmail.com>
2023-01-02 23:10:19 -0300

    Created colapse to sidebar

--- src/components/Sidebar/SidebarItem.tsx
+++ src/components/Sidebar/SidebarItem.tsx
@@ -1,21 +1,45 @@
-import { ListItem, ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
+import { ListItem, ListItemButton, ListItemIcon, ListItemText, Tooltip, useTheme } from "@mui/material";
 import { ReactNode } from "react";
 
 interface SidebarItemProps {
   url: string;
   icon: ReactNode;
   name: string;
+  toggleSidebar: boolean;
 }
 
-export function SidebarItem({ url, icon, name }: SidebarItemProps) {
+export function SidebarItem({ url, icon, name, toggleSidebar }: SidebarItemProps) {
+  const theme = useTheme()
+
   return (
-    <ListItem disablePadding>
-      <ListItemButton component="a" href={url}>
-        <ListItemIcon>
-          {icon}
-        </ListItemIcon>
-        <ListItemText primary={name} />
-      </ListItemButton>
-    </ListItem>
+    <>
+      {toggleSidebar ? (
+        <ListItem sx={{ width: !toggleSidebar ? "50px" : theme.shape }} disablePadding>
+          <ListItemButton component="a" href={url}>
+            <ListItemIcon>
+              {icon}
+            </ListItemIcon>
+            <ListItemText
+              sx={{ display: !toggleSidebar ? "none" : "flex" }}
+              primary={name}
+            />
+          </ListItemButton>
+        </ListItem>
+      ) : (
+        <Tooltip title={name} >
+          <ListItem sx={{ width: !toggleSidebar ? "50px" : theme.shape }} disablePadding>
+            <ListItemButton component="a" href={url}>
+              <ListItemIcon>
+                {icon}
+              </ListItemIcon>
+              <ListItemText
+                sx={{ display: !toggleSidebar ? "none" : "flex" }}
+                primary={name}
+              />
+            </ListItemButton>
+          </ListItem>
+        </Tooltip>
+      )}
+    </>
   )
 }
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,17 +1,23 @@
-import { Box, List } from "@mui/material";
+import { Box, Button, List, Switch, Typography } from "@mui/material";
 
 import { SidebarItem } from "./SidebarItem";
 import { sidebarOptions, SidebarOptionsProps } from "./sidebarOptions";
+import { useState } from "react";
 
 export function Sidebar() {
+  const [toggleSidebar, setToggleSidebar] = useState(true)
+
   return (
-    <Box flex={1} p={2} sx={{ display: { xs: "none", sm: "block" }}}>
+    <Box flex={toggleSidebar ? 1 : 0.20} p={2} sx={{ display: { xs: "none", sm: "block" } }}>
       <Box position="fixed">
-         <List>
+        <List>
           {sidebarOptions.map((option: SidebarOptionsProps) => (
-            <SidebarItem name={option.name} icon={option.icon} url={option.url} />
+            <SidebarItem name={option.name} icon={option.icon} url={option.url} toggleSidebar={toggleSidebar} />
           ))}
         </List>
+        <Button sx={{ display: "flex", alignItems: "center", width: "100%" }}>
+          <Switch checked={toggleSidebar} onClick={() => setToggleSidebar(!toggleSidebar)} />
+        </Button>
       </Box>
     </Box>
   )
